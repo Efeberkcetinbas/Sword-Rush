5beda2cf358c13bf67d1f6cd6d966f853d557b4f
Efe Berk Çetinbaş <berkefefast@gmail.com>
2022-10-24 04:28:44 +0300

    Boundaries Added

---
+++ Assets/Scripts/LevelsNavControl.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class LevelsNavControl : MonoBehaviour
+{
+    public NavMeshSurface[] surfaces;
+
+    void Start()
+    {
+        for (int i = 0; i < surfaces.Length; i++)
+        {
+            surfaces[i].BuildNavMesh();
+        }
+    }
+
+    
+
+}
